d42d3bfb1a5d6f0d7d274131b680047de466a9b9
bdm <czahrien@czahrien-virtual-machine.(none)>
2011-03-17 13:00:20 -0400

    Added my automagical font-making tool!

---
+++ util/font_maker/font.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( int argc, char** argv ) {
+    FILE* font = fopen( argv[1], "r" );
+    if( !font ) {
+        perror( "fopen" );
+        return 1;
+    }
+
+    int width;
+    int height;
+    fscanf( font, "%d %d", &width, &height );
+    printf( "#include <gs_font.h>\n\n" ); 
+    printf( "pixel_t font[256][%d][%d];\n", width, height );
+    int num;
+    printf( "void _init_font() {\n" );
+    while( fscanf( font, "%d", &num ) != EOF ) {
+        int i = 0;
+        int j = 0;
+        char c = 0;
+        int output;
+        for( i = 0; i < height; ++i ) {
+            for( j = 0; j < width; ++j ) {
+                do {
+                    fscanf( font, "%c", &c );
+                } while( c != '0' && c != '1' ); 
+                if( c == '1' ) {
+                   output = -1;
+                } else {
+                   output = 0;
+                }   
+                printf( "  font[%d][%d][%d] = 0x%x\n", num, i, j, output );
+            }
+        }
+        printf( "\n" );
+    }
+    printf( "}\n" );
+
+
+    fclose( font );
+    return 0;
+}
