80a06f5967eac3257dfcbf937312b3f133df261e
bdm <czahrien@czahrien-virtual-machine.(none)>
2011-03-18 03:15:25 -0400

    Modified font.c to split out a file with an array initialized to contain fonts.
    font.txt now contains numbers and both cases of letters.

--- util/font_maker/font.c
+++ util/font_maker/font.c
@@ -6,40 +6,97 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+int width;
+int height;
+FILE* font;
+
+void print_char(int c) {
+    int j,k,test=0; 
+    printf( "//Character 0x%02x (dec %d)\n{", c,c );
+    for( j = 0; j < height; ++j ) {
+        printf( "{" );
+        for( k = 0; k < width; ++k ) {
+            char x;
+            ++test;
+            do {
+                fscanf(font, "%c", &x );
+            } while( x != '0' &&  x != '1' );
+            if( x == '0' ) {
+                c=0;
+            } else {
+                c=0xFFFF;
+            }
+            printf("0x%04x",c);
+            if(k < width - 1) printf(", ");
+        }
+        printf ("}");
+        if( j < height - 1 ) {
+            printf( ", \n");
+        } else {
+            if( c < 255 ) {
+                printf( "},//%d\n",test );
+            } else {
+                printf( "}//%d\n",test );
+            }
+        }
+    }
+}
+
+void print_blank_char(int c) {
+    int j,k; 
+    printf( "//Character 0x%02x (dec %d)\n{", c,c );
+    for( j = 0; j < height; ++j ) {
+        printf( "{" );
+        for( k = 0; k < width; ++k ) {
+            printf("0x0000");
+            if(k < width - 1) printf(", ");
+        }
+        printf ("}");
+        if( j < height - 1 ) {
+            printf( ", \n");
+        } else {
+            if( c < 255 ) {
+                printf( "},\n" );
+            } else {
+                printf( "}\n" );
+            }
+        }
+    }
+}
+
 
 int main( int argc, char** argv ) {
-    FILE* font = fopen( argv[1], "r" );
+    font = fopen( argv[1], "r" );
     if( !font ) {
         perror( "fopen" );
         return 1;
     }
-
-    int width;
-    int height;
-    fscanf( font, "%d %d", &width, &height );
+    int i = 0;
+    int j = 0;
+    int k = 0;
+        
+    printf( "/**\n * @author Benjamin Mayes and font_maker (written by Benjamin Mayes)\n * @description This is an automatically generated file that generates fonts \n *used by the kernel early on for debugging output.\n */\n\n");
+    printf( "#include \"gs_font.h\"\n\n" );
+    fscanf( font, "%d %d", &height, &width );
     int num;
-    printf( "void _init_font() {\n" );
+    printf( "uint16_t font[128][%d][%d] = {\n", height, width );
+    int last_num = 0;
     while( fscanf( font, "%d", &num ) != EOF ) {
-        int i = 0;
-        int j = 0;
-        char c = 0;
-        short output;
-        for( i = 0; i < height; ++i ) {
-            for( j = 0; j < width; ++j ) {
-                do {
-                    fscanf( font, "%c", &c );
-                } while( c != '0' && c != '1' ); 
-                if( c == '1' ) {
-                   output = -1;
-                } else {
-                   output = 0;
-                }   
-                printf( "  font[%d][%d][%d] = 0x%x;\n", num, j, i, output&0xFFFF );
+        if( last_num < num - 1 ) {
+            for( i = last_num; i < num; ++i ) {
+                print_blank_char(i);
             }
         }
-        printf( "\n" );
+        last_num = num + 1;
+        print_char(num);
     }
-    printf( "}\n" );
+
+    ++num;
+    for( i = num; i < 128; ++i ) {
+        print_blank_char(i);
+    }
+        
+    printf( "};\n" );
 
 
     fclose( font );
