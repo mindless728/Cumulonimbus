96453409baa765a67ffdfede75d6290b3315bbe4
Benjamin D. Mayes <bdm8233@rit.edu>
2011-04-08 06:08:42 -0400

    Added some more font characters, commented the screen system calls, caught more potential misuses of the screen system calls, and added some more comments in the screen files.

--- src/kernel/screen.c
+++ src/kernel/screen.c
@@ -8,6 +8,9 @@ static screen_qnode_t _screen_nodes[NUM_SCREENS];
 screen_queue_t _unused_screens;
 screen_queue_t _active_screens;
 
+/**
+ * Removes the first screen from the screen queue.
+ */
 static screen_qnode_t* _dequeue( screen_queue_t* q ) {
     if( !q->length ) return 0;
     screen_qnode_t* n = q->first;
@@ -17,6 +20,10 @@ static screen_qnode_t* _dequeue( screen_queue_t* q ) {
     return n;
 }
 
+/**
+ * Removes a node that is in the screen queue from somewhere within the screen
+ * queue.
+ */
 static screen_qnode_t* _remove_node( screen_queue_t* q, screen_descriptor_t sd ) {
     screen_qnode_t* node = q->first;
     screen_qnode_t* prev = 0;
@@ -45,33 +52,44 @@ static screen_qnode_t* _remove_node( screen_queue_t* q, screen_descriptor_t sd )
     return node;
 }
 
+/**
+ * Queue a node up in the screen queue.
+ */
 static void _enqueue( screen_queue_t* q, screen_qnode_t* n ) {
+    // the node will be at the end so it shouldn't have a next node
     n->next = 0;
     if( q->length) {
-        q->last->next = n;
-        q->last = n;
+        // new node will not the be the first node
+        q->last->next = n; // the old last node's next node is the new node
+        q->last = n; // the queue's last node is the new node
     } else {
+        // new node will be first (and only) element in the queue after addition
         q->first = n;
         q->last = n;
     }
+
+    // increment the queue's length
     ++(q->length);
 }
 
+// clear a screen
 static void _clear_screen( screen_t* screen ) {
     _memclr( (void*)screen,sizeof(screen_t) );
-    screen->owner = -1;
+    screen->owner = 0;
     screen->draw_mode = GS_DRAW_MODE_FLAT;
 }
 
+// _screen_init
 void _screen_init() {
     int i;
-    c_printf( "Clearing screens...\n" );
+    c_printf( "====== BEGIN SCREEN INIT ======\n" );
+    c_printf( "Initalizing screens...\n" );
     for( i = 0; i < NUM_SCREENS; ++i ) {
         _screen_nodes[i].descriptor = i;
-        _clear_screen( &_screens[i] );
+        _screens[i].owner = 0;
     }
 
-    c_printf( "Initializing queues...\n" );
+    c_printf( "Initializing screen queues...\n" );
     // initialize the queues
     _unused_screens.first = &_screen_nodes[0];
     _unused_screens.last = &_screen_nodes[NUM_SCREENS-1];
@@ -86,16 +104,21 @@ void _screen_init() {
         _screen_nodes[i].descriptor = i;
         _screen_nodes[i].next = &_screen_nodes[i+1];
     }
+    c_printf( "======= END SCREEN INIT =======\n" );
 }
 
+// _screen_dequeue
 screen_descriptor_t _screen_dequeue() {
     screen_qnode_t* node = _dequeue( &_unused_screens );
     if( !node ) return E_EMPTY;
     _enqueue( &_active_screens, node );
+    // prep the screen for use by a process
     _clear_screen( &_screens[node->descriptor] );
+    // return the descriptor
     return node->descriptor;
 }
 
+// _screen_enqueue
 status_t _screen_enqueue( screen_descriptor_t sd ) {
     screen_qnode_t* node = _remove_node( &_active_screens, sd ); 
     if( node ) {
