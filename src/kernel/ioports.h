438550694a2058f61767bcebe15318585dce73d9
Colin Alexander Barr <colin@colin-dev.(none)>
2011-04-28 20:57:19 -0400

    added stuff

---
+++ src/kernel/ioports.h
@@ -0,0 +1,10 @@
+#ifndef IOPORTS_H
+#define IOPORTS_H
+
+#include <types.h>
+
+void ioports_init(void);
+uint16_t allocate_ports(uint16_t amount);
+uint16_t allocate_ports_align(uint16_t amount, uint16_t align);
+
+#endif
