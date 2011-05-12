f552176ba2d811b8734a8a0e89cb02ad9294ce6e
Benjamin D. Mayes <bdm8233@rit.edu>
2011-05-11 22:12:29 -0400

    Fixed a bug where I forgot to take averages. I am too tired to be a good coder :(

--- src/kernel/screen_manager.c
+++ src/kernel/screen_manager.c
@@ -13,12 +13,9 @@ void _draw_screen_manager(void) {
     setscreen( newscreen );
     switchscreen( newscreen );
     screen_t* s = _screens;
-    int r;
-    int c;
-    int x;
-    int y;
-    int a;
-    int b;
+    int r, c;
+    int x, y;
+    int a, b;
     for( c = 0; c < 4; ++c ) {
         for( r = 0; r < 4; ++r ) {
             gs_framebuffer_t* current = &(s->fb);
@@ -27,16 +24,16 @@ void _draw_screen_manager(void) {
                     int avgR = 0;
                     int avgG = 0;
                     int avgB = 0;
-                    for( b = 0; b < 1; ++b ) {
-                        for( a = 0; a < 1; ++a ) {
+                    for( b = 0; b < 4; ++b ) {
+                        for( a = 0; a < 4; ++a ) {
                            avgR += EXTRACT_RED(s->fb.buffer[a+4*y][b+4*x]);
                            avgG += EXTRACT_GREEN(s->fb.buffer[a+4*y][b+4*x]);
                            avgB += EXTRACT_BLUE(s->fb.buffer[a+4*y][b+4*x]);
                         }
                     }
-                    avgR >> 4;
-                    avgG >> 4;
-                    avgB >> 4;
+                    avgR >>= 4;
+                    avgG >>= 4;
+                    avgB >>= 4;
                     gs_draw_pixel( c*320 + x, r*256 + y, CREATE_PIXEL( avgR, avgG, avgB ) ); 
                     //gs_draw_pixel( x, y, CREATE_PIXEL( avgR, avgG, avgB ) ); 
                 }
