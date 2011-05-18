ba3e861f3670b8291fe3bc7d300b09b79a3b47eb
Benjamin D. Mayes <bdm8233@rit.edu>
2011-05-18 13:50:22 -0400

    Changed linking order and added comments to screen.h

--- src/kernel/screen.h
+++ src/kernel/screen.h
@@ -17,19 +17,20 @@ extern handle_t active_screen;
 // the current PCB
 extern pcb_t* _current;
 
+// typedef of the qnode type so that it can be used within its own structure
 typedef struct _screen_qnode_t screen_qnode_t;
 
-// a node that holds a screen handle
+// a lightweight node that holds a screen handle
 struct _screen_qnode_t {
-    handle_t handle;
-    screen_qnode_t* next;
+    handle_t handle; // handle associated with the qnode
+    screen_qnode_t* next; // the next qnode in the linked data structure
 };
 
 // a queue of screens
 typedef struct _screen_queue_t {
-    screen_qnode_t* first;
-    screen_qnode_t* last;
-    uint_t length;
+    screen_qnode_t* first;  // front of the queue
+    screen_qnode_t* last;   // back of the queue
+    uint_t length;  // number of queued elements
 } screen_queue_t;
 
 // circular queue-type structure used for keyboard and mouse input
@@ -42,13 +43,14 @@ typedef struct _screen_input_buffer_t {
 
 // the screen data structure itself
 typedef struct _screen_t {
-    gs_framebuffer_t fb;
-    gs_draw_mode_t draw_mode;
-    pid_t owner;
-    screen_input_buffer_t kb_buf;
-    screen_input_buffer_t mouse_buf;
+    gs_framebuffer_t fb;    // the framebuffer
+    gs_draw_mode_t draw_mode;   // the drawing mode for textures onto the fb
+    pid_t owner;                // the owner of this screen
+    screen_input_buffer_t kb_buf;   // the keyboard buffer
+    screen_input_buffer_t mouse_buf;    // the mouse buffer
 } screen_t;
 
+// declaration of the array containing all the intial screens
 extern screen_t _screens[NUM_SCREENS];
 
 /**
