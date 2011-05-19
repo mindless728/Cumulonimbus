c1458a944939d946efa4b8b81ee04df890f647cc
Benjamin D. Mayes <bdm8233@rit.edu>
2011-05-19 10:44:22 -0400

    Comments and merges.

--- src/kernel/deadspace.c
+++ src/kernel/deadspace.c
@@ -1,1 +1,11 @@
+/**
+ * File: deadspace.c
+ * Author: Benjamin David Mayes <bdm8233@rit.edu>
+ * Description: Our kernel starts at 0x10000 and grows upwards as more is added
+ * and this is a problem. The range 0xA0000-0x100000 is a reserved/legacy memory
+ * range and is read-only in some portions. This was causing corruption of BSS
+ * data in that spot. Try removing this from being linked into the kernel,
+ * things WILL break.
+ */
+
 char deadspace[1024][1024];
